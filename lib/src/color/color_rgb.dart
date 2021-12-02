245c47d57567b85515ac6e5c034dd46a00f0208e
jimmyff <jimmy@rocketware.co.uk>
2021-12-02 17:38:24 +0000

    Fix typ in comments

--- lib/src/color/color_rgb.dart
+++ lib/src/color/color_rgb.dart
@@ -155,7 +155,7 @@ class ColorRgb {
   /// Blue component
   final double blue;
 
-  /// Get the int32 representation of this color (ARGB compatible with flutter)
+  /// Get the int32 representation of this color (RGBA compatible with flutter)
   int toInt() =>
       ((((alpha * 255.0).round() & 0xff) << 24) |
           (((red * 255.0).round() & 0xff) << 16) |
